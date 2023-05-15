35ed22d710de83ffb3ddaa329efc788d30a3aad6
Andrey Wilmsen de Paula <71944788+andreywilmsen@users.noreply.github.com>
2023-05-15 14:54:34 -0300

    Exercício 8 ( Conclusão mural de avisos)

--- 8 - Express JS/5 - Mural de avisos/index.js
+++ 8 - Express JS/5 - Mural de avisos/index.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path')
+const apiRoute = require('./routes/api');
 const PORT = 8080;
-const posts = require('./model/posts')
 
+app.use("/api",apiRoute);
+app.use("/",express.static(path.join(__dirname,"public")));
 
-app.get("/all",(req,res) => {
-    res.json(JSON.stringify(posts.getAll()));
-});
-
-app.post("/new",express.json(), (req,res) => {
-    let title = req.body.title;
-    let description = req.body.description;
-    posts.newPost(title, description);
-    res.send("Post adicionado com sucesso!");
-});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
